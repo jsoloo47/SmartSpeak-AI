7236d7d4dfa2c8e27a48dd5d742ff6661eae9c32
Juan Solano <juansolano99.js@gmail.com>
2024-02-25 22:24:35 -0800

    Interview Prep Gangg

---
+++ app/recording/[id]/page.tsx
@@ -0,0 +1,19 @@
+import { api } from '@/convex/_generated/api';
+import { preloadQuery } from 'convex/nextjs';
+import { getAuthToken } from '@/app/auth';
+import RecordingPage from './recording';
+import Header from '@/components/ui/Header';
+
+const Page = async ({ params }: { params: { id: string } }) => {
+  const id = params.id as any;
+  const token = await getAuthToken();
+  const preloadedNote = await preloadQuery(
+    api.notes.getNote,
+    { id },
+    { token },
+  );
+
+  return <RecordingPage preloadedNote={preloadedNote} />;
+};
+
+export default Page;
